6f5103b8da134f593066a7b8c91a2dfe1c99b999
generyand <110978525+generyand@users.noreply.github.com>
2023-12-28 09:31:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,84 @@
-const textRespond = document.querySelector(".text-respond");
-const buttons = document.querySelectorAll(".button");
-const pickBtn = document.querySelector(".pick-btn");
-const respondContainer = document.querySelector(".respond-container");
+(function () {
+  "use strict";
 
-const mangoFloat = [
-  {
-    class: "cheap",
-    innerText: "Ikaw na bahala assemble ana. Daku naka. 😊",
-    img: [
-      "images/condensed-milk.webp",
-      "images/grahams-cracker.webp",
-      "images/grahams-powder.webp",
-      "images/mango.webp",
-    ],
-  },
-  {
-    class: "regular",
-    innerText: "Wieee! Here's your Regular Mango Float. Hope you like it! 🥰",
-    img: ["images/regular.webp"],
-  },
-  {
-    class: "premium",
-    innerText:
-      "Charannnnn! Here's your Premium Mango Float. 🤩 Enjoy while it lasts, bai!",
-    img: ["images/premium.webp"],
-  },
-];
+  const textRespond = document.querySelector(".text-respond");
+  const buttons = document.querySelectorAll(".button");
+  const pickBtn = document.querySelector(".pick-btn");
+  const respondContainer = document.querySelector(".respond-container");
 
-for (const button of buttons) {
-  button.addEventListener("click", () => {
-    pickBtn.classList.add("active");
+  const mangoFloat = [
+    {
+      class: "cheap",
+      innerText: "Ikaw na bahala assemble ana. Daku naka. 😊",
+      img: [
+        "images/condensed-milk.webp",
+        "images/grahams-cracker.webp",
+        "images/grahams-powder.webp",
+        "images/mango.webp",
+      ],
+    },
+    {
+      class: "regular",
+      innerText: "Wieee! Here's your Regular Mango Float. Hope you like it! 🥰",
+      img: ["images/regular.webp"],
+    },
+    {
+      class: "premium",
+      innerText:
+        "Charannnnn! Here's your Premium Mango Float. 🤩 Enjoy while it lasts, bai!",
+      img: ["images/premium.webp"],
+    },
+  ];
 
-    for (const btn of buttons) {
-      btn.classList.remove("clicked");
-    }
-
-    button.classList.add("clicked");
-  });
-}
-
-pickBtn.addEventListener("click", () => {
   for (const button of buttons) {
-    if (button.classList.contains("clicked")) {
-      const pickedChoice = mangoFloat.filter(
-        (arr) => button.id == arr["class"]
-      );
+    button.addEventListener("click", () => {
+      pickBtn.classList.add("active");
 
-      let imageElements = pickedChoice[0].img.map((imageSource) => {
-        return `<img src=${imageSource} alt="">`;
-      });
+      for (const btn of buttons) {
+        btn.classList.remove("clicked");
+      }
 
-      respondContainer.innerHTML = `
+      button.classList.add("clicked");
+    });
+  }
+
+  pickBtn.addEventListener("click", () => {
+    for (const button of buttons) {
+      if (button.classList.contains("clicked")) {
+        const pickedChoice = mangoFloat.filter(
+          (arr) => button.id == arr["class"]
+        );
+
+        let imageElements = pickedChoice[0].img.map((imageSource) => {
+          return `<img src=${imageSource} alt="">`;
+        });
+
+        respondContainer.innerHTML = `
         <p class="text-respond">${pickedChoice[0].innerText}</p>
         <div class="img-container">
           ${imageElements.join("")}
         </div>`;
-      button.classList.remove("clicked");
-    }
-  }
-  pickBtn.classList.remove("active");
-});
-
-document.addEventListener("click", function (event) {
-  const isClickedOutsideChoices = () => {
-    for (const button of buttons) {
-      if (button.contains(event.target)) {
-        return false;
+        button.classList.remove("clicked");
       }
     }
-    return true;
-  };
-
-  if (isClickedOutsideChoices()) {
-    for (const button of buttons) {
-      button.classList.remove("clicked");
-    }
     pickBtn.classList.remove("active");
-  }
-});
+  });
+
+  document.addEventListener("click", function (event) {
+    const isClickedOutsideChoices = () => {
+      for (const button of buttons) {
+        if (button.contains(event.target)) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    if (isClickedOutsideChoices()) {
+      for (const button of buttons) {
+        button.classList.remove("clicked");
+      }
+      pickBtn.classList.remove("active");
+    }
+  });
+})();
